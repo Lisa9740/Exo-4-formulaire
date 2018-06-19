4f6b06dc6a0d883acde9c0393dac53fda168ec3c
Lisa9740 <barretalison@gmail.co>
2018-06-19 09:27:42 +0400

    finalisation

--- myapp/public/formulaire.js
+++ myapp/public/formulaire.js
@@ -10,7 +10,7 @@ $(function () {
 
                 $.post(url, { nbr1: nbr1, nbr2: nbr2 }, function(data){
 
-                $("#result").append(data);
+                $("#result").append("<p class='display-3'>" + data + "</p>");
               
                 });
 
